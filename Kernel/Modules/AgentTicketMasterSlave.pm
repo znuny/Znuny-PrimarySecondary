950827a728f15d7bd5df4f886bbcbe40df9d9d8f
Roy Kaldung <rk@znuny.com>
2022-03-18 09:31:28 +0100

    Ported to Znuny 6.3 and applied CodePolicy

--- Kernel/Modules/AgentTicketMasterSlave.pm
+++ Kernel/Modules/AgentTicketMasterSlave.pm
@@ -1,6 +1,6 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
-# Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021-2022 Znuny GmbH, https://znuny.org/
 # --
 # $origin: otrs - 61764fc3cfd0e81cd5f6dbcf08115e12240f412d - Kernel/Modules/AgentTicketActionCommon.pm
 # --
