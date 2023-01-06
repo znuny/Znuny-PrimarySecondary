ba3d09b62a38a054325b4b81e68cab60f570ed3b
Niklas Schmitt <ns@znuny.com>
2023-01-06 11:12:23 +0100

    Applied ZnunyCodePolicy.

--- Kernel/Modules/AgentTicketMasterSlave.pm
+++ Kernel/Modules/AgentTicketMasterSlave.pm
@@ -1,6 +1,6 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
-# Copyright (C) 2021-2022 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Znuny GmbH, https://znuny.org/
 # --
 # $origin: znuny - 460ef44565300c6b979b0743833e3800fdbebf81 - Kernel/Modules/AgentTicketActionCommon.pm
 # --
